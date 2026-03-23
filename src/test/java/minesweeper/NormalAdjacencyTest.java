1418766a122e0020b22245a152a16c0e7134ae46
Kristin Off <krof5695@colorado.edu>
2026-03-23 16:36:54 -0600

    Worked on designing test case for normal adjacency pattern.

--- src/test/java/minesweeper/NormalAdjacencyTest.java
+++ src/test/java/minesweeper/NormalAdjacencyTest.java
@@ -2,9 +2,19 @@ package minesweeper;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class NormalAdjacencyTest {
     @Test
     public void normalAdjacencyPatternsTest(){
         //creates map with normal adjacency pattern
+        Map map = Map.getBuilder(new TileFactory("Normal"))
+                .create3x3Grid()
+                .placeBomb(0,1)
+                .placeBomb(1,2)
+                .placeBomb(2,0)
+                .build();
+
+        assertEquals(3,map.getTile(1,1).countAdjacentBombs());
     }
 }
