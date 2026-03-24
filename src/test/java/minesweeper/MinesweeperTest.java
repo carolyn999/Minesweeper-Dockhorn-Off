58e6473378d4995a3cd7b961392f3d39c100a0a5
Kristin Off <krof5695@colorado.edu>
2026-03-23 23:07:08 -0600

    Test for game ending implemented.

--- src/test/java/minesweeper/MinesweeperTest.java
+++ src/test/java/minesweeper/MinesweeperTest.java
@@ -1,9 +1,23 @@
 package minesweeper;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class MinesweeperTest {
     @Test
     public void gameWin(){
         //game detects win and exits when all non-bomb tiles are revealed
+        Map map = Map.getBuilder(new TileFactory())
+                .useAdjacencyPattern("Knight")
+                .create3x3Grid()
+                .placeBomb(0,0)
+                .placeBomb(0,2)
+                .placeBomb(2,0)
+                .build();
+
+        Minesweeper minesweeper = new Minesweeper(map);
+        minesweeper.playGame();
+        assertTrue(minesweeper.isOver());
     }
 
     @Test
