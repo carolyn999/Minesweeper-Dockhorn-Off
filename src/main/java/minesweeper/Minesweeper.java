7d8b3eeaf15d28ce09619bf5f65970ab5cd3b07e
Kristin Off <krof5695@colorado.edu>
2026-04-15 22:10:01 -0600

    Created methods for compilation so tests could be run (NormalAdjacencyTest is passing).

--- src/main/java/minesweeper/Minesweeper.java
+++ src/main/java/minesweeper/Minesweeper.java
@@ -3,6 +3,31 @@ package minesweeper;
 public class Minesweeper implements ITilesGame {
     private boolean gameWon;
     private boolean gameOver;
+    private Map map;
 
+    public Minesweeper(Map map)
+    {
+        this.map = map;
+    }
 
+    @Override
+    public void attach()
+    {
+
+    }
+
+    @Override
+    public void detach()
+    {
+
+    }
+
+    public void playGame()
+    {
+    }
+
+    public boolean isOver()
+    {
+        return false; // Placeholder for compilation to run tests.
+    }
 }
