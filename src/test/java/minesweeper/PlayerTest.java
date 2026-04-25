030613d762efca7cedb182f94f226aad4415a944
Kristin Off <krof5695@colorado.edu>
2026-04-25 11:41:47 -0600

    Removed commented code.

--- src/test/java/minesweeper/PlayerTest.java
+++ src/test/java/minesweeper/PlayerTest.java
@@ -6,68 +6,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class PlayerTest {
 
-//    @Test
-//    void testRevealCommandInput()
-//    {
-//        Scanner scanner = new Scanner("r\n1\n2\n");
-//
-//        Player player = new Player(scanner);
-//        PlayerCommand command = player.getCommand();
-//
-//        assertFalse(command.isQuit());
-//        assertEquals("reveal at row 1, col 2", command.toString());
-//    }
-//
-//    @Test
-//    void testFlagCommandInput()
-//    {
-//        Scanner scanner = new Scanner("f\n0\n1\n");
-//        Player player = new Player(scanner);
-//        PlayerCommand command = player.getCommand();
-//
-//        assertFalse(command.isQuit());
-//        assertEquals("flag at row 0, col 1", command.toString());
-//    }
-//
-//    @Test
-//    void testQuitCommandInput()
-//    {
-//        Scanner scanner = new Scanner("q\n");
-//        Player player = new Player(scanner);
-//        PlayerCommand command = player.getCommand();
-//        assertTrue(command.isQuit());
-//    }
-//
-//    @Test
-//    void testRevealCommandRevealsTile()
-//    {
-//        Map map = Map.getBuilder(new TileFactory()).useAdjacencyPattern("Normal").create3x3Grid().placeBomb(0, 0).build();
-//
-//        Scanner scanner = new Scanner("r\n1\n1\n");
-//        Player player = new Player(scanner);
-//
-//        PlayerCommand command = player.getCommand();
-//        command.execute(map);
-//
-//        assertTrue(map.getTile(1, 1).isRevealedTile());
-//        assertEquals("1", map.getTile(1, 1).toString());
-//    }
-//
-//    @Test
-//    void testFlagCommandFlagsTile()
-//    {
-//        Map map = Map.getBuilder(new TileFactory()).useAdjacencyPattern("Normal").create3x3Grid().build();
-//
-//        Scanner scanner = new Scanner("f\n0\n1\n");
-//        Player player = new Player(scanner);
-//
-//        PlayerCommand command = player.getCommand();
-//        command.execute(map);
-//
-//        assertTrue(map.getTile(0, 1).isFlaggedTile());
-//        assertEquals("F", map.getTile(0, 1).toString());
-//    }
-
     @Test
     void testRevealCommandInput()
     {
