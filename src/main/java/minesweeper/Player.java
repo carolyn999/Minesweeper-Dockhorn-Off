889d3a8aa8e0a3da1d3c81816c49c674edb1edf6
Kristin Off <krof5695@colorado.edu>
2026-04-24 19:09:28 -0600

    Removed commented code.

--- src/main/java/minesweeper/Player.java
+++ src/main/java/minesweeper/Player.java
@@ -15,43 +15,6 @@ public class Player
         this.scanner = scanner;
     }
 
-//    public PlayerCommand getCommand()
-//    {
-//        String action = getAction();
-//        if (action.equals("quit"))
-//        {
-//            return new PlayerCommand("quit", -1, -1);
-//        }
-//        int row = getRow();
-//        int col = getCol();
-//        return new PlayerCommand(action, row, col);
-//    }
-//
-//    private String getAction()
-//    {
-//        while (true)
-//        {
-//            System.out.print("Enter action: reveal (R), flag (F), or quit (Q): ");
-//            String action = scanner.nextLine().trim().toLowerCase();
-//
-//            if (action.startsWith("r"))
-//            {
-//                return "reveal";
-//            }
-//            else if (action.startsWith("f"))
-//            {
-//                return "flag";
-//            }
-//            else if (action.startsWith("q"))
-//            {
-//                return "quit";
-//            }
-//            else
-//            {
-//                System.out.println("Invalid action. Enter R, F, or Q.");
-//            }
-//        }
-//    }
     public PlayerAction getAction() {
         while (true) {
             System.out.print("Enter action: reveal, flag, or quit: ");
